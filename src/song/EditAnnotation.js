d0bfc16d261cd232ad5a31fde9fb1a8f192bb0bb
Carl Dobrovic <cadobrovic@gmail.com>
2019-12-12 12:22:57 -0500

    Incorporated adding tags to EditAnnotation. We may want to update this to instead have tags display as their own chunks on the UI. But this should work.

--- src/song/EditAnnotation.js
+++ src/song/EditAnnotation.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 import WindowTime from './WindowTime'
-import { Button, Icon, TextArea, Form } from 'semantic-ui-react'
+import { Button, Icon, TextArea, Form, Dropdown } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
 
 const EditAnnotationWrapper = styled.div`
@@ -54,6 +54,13 @@ const WindowWrapper = styled.div`
     justify-content: space-between;
 `
 
+const TagsWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    margin-top: 2.5em;
+`
+
 const ButtonWrapper = styled.div`
 	position: absolute;
 	bottom: 1rem;
@@ -64,12 +71,44 @@ const ButtonWrapper = styled.div`
 	width: 50%;
 `
 
+const DropDownBox = styled.div`
+	margin-right: 5px;
+	margin-left: 1.5em;
+`
+
 export default class EditAnnotation extends Component {
 	constructor(props) {
 		super(props)
 	}
 
 	render() {
+
+        const countryOptions = [
+			{ key: 'af', value: 'af', flag: 'af', text: 'Afghanistan' },
+            { key: 'ax', value: 'ax', flag: 'ax', text: 'Aland Islands' },
+            { key: 'al', value: 'al', flag: 'al', text: 'Albania' },
+            { key: 'dz', value: 'dz', flag: 'dz', text: 'Algeria' },
+            { key: 'as', value: 'as', flag: 'as', text: 'American Samoa' },
+            { key: 'ad', value: 'ad', flag: 'ad', text: 'Andorra' },
+            { key: 'ao', value: 'ao', flag: 'ao', text: 'Angola' },
+            { key: 'ai', value: 'ai', flag: 'ai', text: 'Anguilla' },
+            { key: 'ag', value: 'ag', flag: 'ag', text: 'Antigua' },
+            { key: 'ar', value: 'ar', flag: 'ar', text: 'Argentina' },
+            { key: 'am', value: 'am', flag: 'am', text: 'Armenia' },
+            { key: 'aw', value: 'aw', flag: 'aw', text: 'Aruba' },
+            { key: 'au', value: 'au', flag: 'au', text: 'Australia' },
+            { key: 'at', value: 'at', flag: 'at', text: 'Austria' },
+            { key: 'az', value: 'az', flag: 'az', text: 'Azerbaijan' },
+            { key: 'bs', value: 'bs', flag: 'bs', text: 'Bahamas' },
+            { key: 'bh', value: 'bh', flag: 'bh', text: 'Bahrain' },
+            { key: 'bd', value: 'bd', flag: 'bd', text: 'Bangladesh' },
+            { key: 'bb', value: 'bb', flag: 'bb', text: 'Barbados' },
+            { key: 'by', value: 'by', flag: 'by', text: 'Belarus' },
+            { key: 'be', value: 'be', flag: 'be', text: 'Belgium' },
+            { key: 'bz', value: 'bz', flag: 'bz', text: 'Belize' },
+            { key: 'bj', value: 'bj', flag: 'bj', text: 'Benin' },
+        ]
+        
 		return (
 			<EditAnnotationWrapper>
 				<EditAnnotationTitle>Edit Annotation</EditAnnotationTitle>
@@ -97,7 +136,24 @@ export default class EditAnnotation extends Component {
                             <WindowTime />
                         </WindowWrapper>
 						
-						<OptionsText>Tags</OptionsText>
+                        <TagsWrapper>
+                            <OptionsText>Tags</OptionsText>
+                            <DropDownBox>
+                                <Dropdown
+                                    placeholder="Add Tags"
+                                    search
+                                    selection
+                                    multiple
+                                    options={countryOptions}
+                                    onChange={
+                                        this.handleSearchChange
+                                    }
+                                />
+                            </DropDownBox>
+                        </TagsWrapper>
+						
+
+
 						<ButtonWrapper>
 							<Button size="huge" animated>
 								<Button.Content visible>Discard</Button.Content>
