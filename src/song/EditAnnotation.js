d1041db8f0723ca22bbbf2bd62fd01c610c685e4
Carl Dobrovic <cadobrovic@gmail.com>
2019-12-12 11:39:43 -0500

    Added timeline styling. Need to wire function calls and redux interaction. Need to incorporate adding tags.

--- src/song/EditAnnotation.js
+++ src/song/EditAnnotation.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import { Button, Icon, TextArea, Form } from 'semantic-ui-react';
-import 'semantic-ui-css/semantic.min.css';
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import WindowTime from './WindowTime'
+import { Button, Icon, TextArea, Form } from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css'
 
 const EditAnnotationWrapper = styled.div`
 	width: 95%;
@@ -13,20 +14,20 @@ const EditAnnotationWrapper = styled.div`
 	/* background-color: rgb(161, 161, 161); */
 
 	padding: 10px 10px 10px 10px;
-`;
+`
 
 const SideBySideWrapper = styled.div`
 	display: flex;
 	flex-direction: row;
 	justify-content: center;
 	height: 85%;
-`;
+`
 
 const TextFieldWrapper = styled.div`
 	padding-bottom: 10px;
 	width: 100rem;
 	height: 100%;
-`;
+`
 
 const OptionsWrapper = styled.div`
 	width: 100%;
@@ -35,29 +36,37 @@ const OptionsWrapper = styled.div`
 	flex-direction: column;
 	justify-content: flex-start;
 	position: relative;
-`;
+`
 
-const EditAnnotationTitle = styled.h2``;
+const EditAnnotationTitle = styled.h2``
 
-const OptionsText = styled.h2``;
+const OptionsText = styled.h2`
+    width: auto;
+`
 
 const OptionsButton = styled.button`
 	background-color: ${props => props.background};
-`;
+`
+
+const WindowWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+`
 
 const ButtonWrapper = styled.div`
 	position: absolute;
 	bottom: 1rem;
-    left: 2rem;
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-start;
-    width: 50%;
-`;
+	left: 2rem;
+	display: flex;
+	flex-direction: row;
+	justify-content: flex-start;
+	width: 50%;
+`
 
 export default class EditAnnotation extends Component {
 	constructor(props) {
-		super(props);
+		super(props)
 	}
 
 	render() {
@@ -83,29 +92,30 @@ export default class EditAnnotation extends Component {
 					</TextFieldWrapper>
 
 					<OptionsWrapper>
-						<OptionsText>Window</OptionsText>
+                        <WindowWrapper>
+                            <OptionsText>Window</OptionsText>
+                            <WindowTime />
+                        </WindowWrapper>
+						
 						<OptionsText>Tags</OptionsText>
 						<ButtonWrapper>
+							<Button size="huge" animated>
+								<Button.Content visible>Discard</Button.Content>
+								<Button.Content hidden>
+									<Icon name="trash" />
+								</Button.Content>
+							</Button>
 
-                            <Button size='huge' animated>
-                                <Button.Content visible>Discard</Button.Content>
-                                <Button.Content hidden>
-                                    <Icon name='trash' />
-                                </Button.Content>
-                            </Button>
-
-                            <Button positive size='huge' animated>
-                                <Button.Content visible>Save</Button.Content>
-                                <Button.Content hidden>
-                                    <Icon name='save' />
-                                </Button.Content>
-                            </Button>
-
-                            
+							<Button positive size="huge" animated>
+								<Button.Content visible>Save</Button.Content>
+								<Button.Content hidden>
+									<Icon name="save" />
+								</Button.Content>
+							</Button>
 						</ButtonWrapper>
 					</OptionsWrapper>
 				</SideBySideWrapper>
 			</EditAnnotationWrapper>
-		);
+		)
 	}
 }
