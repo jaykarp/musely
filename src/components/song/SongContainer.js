55903f5bc6f272b759f6aafc0abebbe5a8a60acc
Jay Karp <jaykarp2@gmail.com>
2019-12-13 11:44:04 -0500

    worked on waveform and state interation

--- src/components/song/SongContainer.js
+++ src/components/song/SongContainer.js
@@ -24,43 +24,58 @@ class SongContainer extends Component {
         super(props)
         const { dispatch } = this.props
 
-        //Sample Dispatch
-        dispatch(
-            addNote({
-                text: 'Hello World'
-            })
-        )
-        dispatch(
-            updateNote({
-                text: 'Goodbye World',
-                id: 0
-            })
-        )
-        dispatch(
-            addAnnotation({
-                text: 'Hello Annotation',
-                start_time: 110,
-                end_time: 120,
-                tag: 'Crescendo'
-            })
-        )
-        dispatch(
-            updateAnnotation({
-                text: 'Goodbye Annotation',
-                id: 0
-            })
-        )
+        this.handleCursor = this.handleCursor.bind(this)
+        this.handleCursorMove = this.handleCursorMove.bind(this)
+        this.handleRegion = this.handleRegion.bind(this)
+        this.handlePlay = this.handlePlay.bind(this)
+        this.handleSave = this.handleSave.bind(this)
+        this.handleTagChange = this.handleTagChange.bind(this)
+        this.handleTextChange = this.handleTextChange.bind(this)
+        //this.handleTimeChange = this.handleTimeChange.bind(this)
 
         this.state = {
-            selected_groups: [],
             playing: false,
-            pos: 0
+            text: '',
+            start_time: 0,
+            end_time: 0,
+            cursorTime: 0,
+            currentTime: 0,
+            tag: ''
         }
     }
 
-    handlePosChange = e => {
+    handleCursor = data => {
+        let curr = parseInt(data.currentTime)
+        if (data.region) {
+            this.setState({
+                currentTime: curr
+            })
+        } else {
+            this.setState({
+                start_time: curr,
+                end_time: curr + 10,
+                currentTime: curr
+            })
+        }
+    }
+
+    handleCursorMove = currentTime => {
+        let curr = parseInt(currentTime)
         this.setState({
-            pos: e.originalArgs[0]
+            cursorTime: curr
+        })
+    }
+
+    handleRegion = ({ start, end }) => {
+        this.setState({
+            start_time: start,
+            end_time: end
+        })
+    }
+
+    handlePlay = currentTime => {
+        this.setState({
+            currentTime: currentTime
         })
     }
 
@@ -70,20 +85,117 @@ class SongContainer extends Component {
         })
     }
 
+    handleSave = () => {
+        let hasAnnotation = false
+        const { dispatch } = this.props
+        if (hasAnnotation) {
+            // TODO: Implement update annotation
+        } else {
+            dispatch(
+                addAnnotation({
+                    text: this.state.text,
+                    start_time: this.state.start_time,
+                    end_time: this.state.end_time,
+                    tag: this.state.tag
+                })
+            )
+        }
+    }
+
+    handleTagChange = (e, data) => {
+        this.setState({
+            tag: data.value
+        })
+    }
+
+    handleTextChange = (e, data) => {
+        e.preventDefault()
+        this.setState({
+            text: data.value
+        })
+    }
+
+    //methods for attempting to do input
+    //handleTimeChange = timeFields => {
+    //console.log(timeFields)
+    //if (timeFields.sm !== NaN || timeFields.ss !== NaN) {
+    //this.setState({
+    //start_time: this.formatTime(timeFields)
+    //})
+    //} else if (timeFields.em !== NaN || timeFields.es !== NaN) {
+    //this.setState({
+    //end_time: this.formatTime(timeFields)
+    //})
+    //}
+    //}
+
+    //formatTime = ({ sm, ss, em, es }) => {
+    //let { minutes: currsm, seconds: currss } = this.sec_toMS(
+    //this.state.start_time
+    //)
+    //let { minutes: currem, seconds: curres } = this.sec_toMS(
+    //this.state.end_time
+    //)
+
+    //if (sm !== undefined) {
+    //return this.ms_toSec({
+    //minutes: Math.abs(parseInt(currsm) - parseInt(sm)),
+    //seconds: parseInt(currss)
+    //})
+    //} else if (ss !== undefined) {
+    //return this.ms_toSec({
+    //minutes: Math.abs(parseInt(currss) - parseInt(ss)),
+    //seconds: parseInt(currss)
+    //})
+    //} else if (em !== undefined) {
+    //return this.ms_toSec({
+    //minutes: Math.abs(parseInt(currem) - parseInt(em)),
+    //seconds: parseInt(currss)
+    //})
+    //} else if (es !== undefined) {
+    //return this.ms_toSec({
+    //minutes: Math.abs(parseInt(curres) - parseInt(es)),
+    //seconds: parseInt(currss)
+    //})
+    //}
+    //}
+
+    //sec_toMS = time => {
+    //let curr = parseInt(time)
+    //let minutes = Math.floor(curr / 60)
+    //let seconds = curr - minutes * 60
+    //let min_str = minutes.toString()
+    //if (min_str.length < 2) min_str = '0' + min_str
+    //let sec_str = seconds.toString()
+    //if (sec_str.length < 2) sec_str = '0' + sec_str
+    //return { minutes: min_str, seconds: sec_str }
+    //}
+
+    //ms_toSec = ({ minutes, seconds }) => {
+    //return minutes * 60 + seconds
+    //}
+
     render() {
         const { name } = this.props.match.params
-
         return (
             <div>
                 <SongHeader>{name}</SongHeader>
                 <Waveform
                     src={`/${this.props.match.params.name}.mp3`}
-                    pos={this.state.pos}
-                    onPosChange={this.handlePosChange}
-                    playing={this.state.playing}
+                    currentTime={this.state.currentTime}
+                    cursorTime={this.state.cursorTime}
+                    handlePlay={this.handlePlay}
+                    handleCursor={this.handleCursor}
+                    handleCursorMove={this.handleCursorMove}
+                    handleRegion={this.handleRegion}
                 />
-                {/* <SongHeader>Notes</SongHeader> */}
-                <EditAnnotation />
+                <EditAnnotation
+                    handleSave={this.handleSave}
+                    handleTagChange={this.handleTagChange}
+                    handleTextChange={this.handleTextChange}
+                    start_time={this.state.start_time}
+                    end_time={this.state.end_time}
+                />
             </div>
         )
     }
--- src/components/song/WindowTime.js
+++ src/components/song/WindowTime.js
@@ -1,7 +1,7 @@
 // WindowTime.js
 
-import React, { Component, StyleSheet } from 'react';
-import styled from 'styled-components';
+import React, { Component, StyleSheet } from 'react'
+import styled from 'styled-components'
 
 const WTWrapper = styled.div`
     /* display: block; */
@@ -15,7 +15,7 @@ const RowLayout = styled.div`
 `
 
 const singleInput = {
-    textAlign : 'center',
+    textAlign: 'center',
     border: '0',
     fontSize: '25px'
 }
@@ -37,6 +37,11 @@ const Colon = styled.p`
     margin: 0;
 `
 
+const TimeField = styled.div`
+    font-size: 30px;
+    padding: 5px;
+`
+
 // const lineBreak = {
 //     display: 'block',
 //     marginTop: '0.5em',
@@ -47,60 +52,51 @@ const Colon = styled.p`
 //     border-width: 1px;
 // }
 // const SingleInputField = styled.input`
-    
+
 // `
 
 export default class WindowTime extends Component {
+    constructor(props) {
+        super(props)
 
-    
+        //const { start_time, end_time } = this.props
+
+        //this.state = {
+        //sm: this.sec_toMS(start_time).minutes,
+        //ss: this.sec_toMS(start_time).seconds,
+        //em: this.sec_toMS(end_time).minutes,
+        //es: this.sec_toMS(end_time).seconds
+        //}
+    }
+
+    sec_toMS = time => {
+        if (time === NaN) return 0
+        let curr = parseInt(time)
+        let minutes = Math.floor(curr / 60)
+        let seconds = curr - minutes * 60
+        let min_str = minutes.toString()
+        if (min_str.length < 2) min_str = '0' + min_str
+        let sec_str = seconds.toString()
+        if (sec_str.length < 2) sec_str = '0' + sec_str
+        return { minutes: min_str, seconds: sec_str }
+    }
 
     render() {
-        return(
+        const { start_time, end_time } = this.props
+        const { handleTimeChange } = this.props
+        return (
             <WTWrapper>
                 <RowLayout>
+                    <TimeField>{this.sec_toMS(start_time).minutes}</TimeField>
+                    <Colon>:</Colon>
+                    <TimeField>{this.sec_toMS(start_time).seconds}</TimeField>
+                    <Line />
 
-                
-                <input 
-                    defaultValue={this.props.startMinute.length < 2 ? this.props.startMinute + '0' : this.props.startMinute}
-                    maxlength="2"
-                    size="2"
-                    style={singleInput}
-                    onChange={this.props.startMinuteUpdate}
-                />
-                <Colon>:</Colon>
-                <input 
-                    defaultValue={this.props.startSecond}  
-                    maxlength="2"
-                    size="2"
-                    style={singleInput}
-                    onChange={this.props.startSecondUpdate}
-                />
-
-                <Line />
-
-                <input 
-                    defaultValue={this.props.endMinute}
-                    maxlength="2"
-                    size="2"
-                    style={singleInput}
-                    onChange={this.props.endMinuteUpdate}
-                />
-                <Colon>:</Colon>
-                <input 
-                    defaultValue={this.props.endSecond}
-                    maxlength="2"
-                    size="2"
-                    style={singleInput}
-                    onChange={this.props.endSecondUpdate}
-                />
-                
-
+                    <TimeField>{this.sec_toMS(end_time).minutes}</TimeField>
+                    <Colon>:</Colon>
+                    <TimeField>{this.sec_toMS(end_time).seconds}</TimeField>
                 </RowLayout>
             </WTWrapper>
         )
     }
 }
-
-
-
-
